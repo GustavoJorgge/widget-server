4939da830c580e49c9bdd5bd767711abfc42705c
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-08-19 19:52:52 -0300

    new: configure role iam

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -4,6 +4,6 @@ const MAXIMUM_FILE_SIZE_IN_BYTES = 1024 * 1024 * 4; // 4mb
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get("/health", async (request, reply) => {
-    await reply.status(200).send({ message: "OK!" });
+    await reply.status(200).send({ message: "TUDO OK!" });
   });
 }
