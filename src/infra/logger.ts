8ee92160322d3ca2778be1bece0f7271aa40f588
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-10-23 11:08:57 -0300

    new: configurando grafana

---
+++ src/infra/logger.ts
@@ -0,0 +1,23 @@
+import { color } from "motion";
+import pino from "pino";
+
+const log = pino({
+  level: "debug",
+  transport: {
+    targets: [
+      {
+        target: "pino-pretty",
+        level: "error",
+        options: {
+          name: "dev-terminal",
+          colorize: true,
+          levelFirst: true,
+          include: "level,time",
+          translateTime: "yyyy-mm-dd HH:MM:ss Z",
+        },
+      },
+    ],
+  },
+});
+
+export { log };
